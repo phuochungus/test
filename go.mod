716955b654508b9484b8a1b9c95f0597b197031f
Nguyen Phuoc Hung <phuochungus@gmail.com>
2023-07-31 19:48:55 +0700

    add custom message template

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/bwmarrin/snowflake v0.3.0
+	github.com/go-playground/validator/v10 v10.14.0
 	github.com/jackc/pgx/v5 v5.4.2
 )
 
@@ -14,7 +15,6 @@ require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/jackc/puddle/v2 v2.2.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
--- src/middleware/validator.go
+++ src/middleware/validator.go
@@ -1,10 +1,17 @@
 package middleware
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
+
+type ErrMsg struct {
+	Value string `json:"value"`
+	Tag   string `json:"tag"`
+}
 
 func ValidateBody[T any]() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
@@ -14,8 +21,17 @@ func ValidateBody[T any]() gin.HandlerFunc {
 			ctx.Next()
 			return
 		} else {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
+			if validationErrs, ok := err.(validator.ValidationErrors); ok {
+				var t []ErrMsg
+				for _, v := range validationErrs {
+					t = append(t, ErrMsg{Tag: v.Tag(), Value: v.Field()})
+				}
+				ctx.JSON(http.StatusBadRequest, gin.H{"msg": "supprite"})
+				return
+			} else if marshallingErr, ok := err.(*json.UnmarshalTypeError); ok {
+				ctx.JSON(http.StatusBadRequest, marshallingErr.Type.String())
+
+			}
 		}
 	}
 }
