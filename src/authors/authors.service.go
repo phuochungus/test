1c4b458bcd0f60b67f9788235e118ca9ce1af16c
Nguyen Phuoc Hung <phuochungus@gmail.com>
2023-07-31 13:03:33 +0700

    finish CRUD

--- src/authors/authors.service.go
+++ src/authors/authors.service.go
@@ -3,6 +3,7 @@ package authors
 import (
 	"context"
 	sqlcGen "root/db/generated"
+	idGen "root/src/id_generator"
 	"root/src/pool"
 )
 
@@ -17,6 +18,51 @@ func FindAll() (authors []sqlcGen.Author, err error) {
 	return
 }
 
-func CreateOne(createDto sqlcGen.CreateAuthorParams) {
+func FindOne(id int64) (author sqlcGen.Author, err error) {
+	conn, err := pool.GetConnection()
+	defer conn.Release()
+	if err != nil {
+		return
+	}
 
+	q := sqlcGen.New(conn)
+
+	author, err = q.GetAuthor(context.Background(), id)
+	return
+}
+
+func CreateOne(name string) (author sqlcGen.Author, err error) {
+	conn, err := pool.GetConnection()
+	defer conn.Release()
+	if err != nil {
+		return
+	}
+
+	id := idGen.GenId()
+	q := sqlcGen.New(conn)
+
+	author, err = q.CreateAuthor(context.Background(), sqlcGen.CreateAuthorParams{ID: id, Name: name})
+	return
+}
+
+func UpdateAuthor(id int64, name string) (author sqlcGen.Author, err error) {
+	conn, err := pool.GetConnection()
+	defer conn.Release()
+	if err != nil {
+		return
+	}
+	q := sqlcGen.New(conn)
+	author, err = q.UpdateAuthor(context.Background(), sqlcGen.UpdateAuthorParams{ID: id, Name: name})
+	return
+}
+
+func DeleteAuthor(id int64) (err error) {
+	conn, err := pool.GetConnection()
+	defer conn.Release()
+	if err != nil {
+		return
+	}
+	q := sqlcGen.New(conn)
+	err = q.DeleteAuthor(context.Background(), id)
+	return
 }
